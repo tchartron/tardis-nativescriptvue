96198fc49a77bf2ae5d12cd183ccad582179ec08
Thomas Chartron <thomas.chartron@gmail.com>
2019-05-05 18:09:57 +0200

    :wrench: backend Api file

---
+++ app/services/backend-api.js
@@ -0,0 +1,25 @@
+import { Kinvey } from "kinvey-nativescript-sdk";
+Kinvey.init({
+    appKey: "kid_SyY8LYO8M",
+    appSecret: "09282985d7c540f7b076a9c7fd884c77"
+});
+
+export default class BackendService {
+
+    isLoggedIn() {
+        return !!Kinvey.User.getActiveUser();
+    }
+
+    login(user) {
+        return Kinvey.User.login(user.email, user.password);
+    }
+
+    logout() {
+        return Kinvey.User.logout();
+    }
+
+    register(user) {
+        return Kinvey.User.signup({ username: user.email, password: user.password });
+    }
+}
+
