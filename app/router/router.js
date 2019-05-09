d1b26770c0058e8bae049ea0edb238c405cd5a40
Thomas Chartron <tchartron@vinatis.com>
2019-05-09 16:41:06 +0200

    :bug: Fixing router silly mistake

--- app/router/router.js
+++ app/router/router.js
@@ -1,7 +1,7 @@
 import Login from '../components/Login'
 import Home from '../components/Home'
 import Company from '../components/Company'
-import Company from '../components/Task'
+import Task from '../components/Task'
 
 const router = {
    login: Login,
